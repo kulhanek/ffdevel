5a3ae79427206715a431d0729659c75edeeb7bf1
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-12-30 13:07:10 +0100

    nlopt

--- src/lib/ffdevel/parameters/parameters.f90
+++ src/lib/ffdevel/parameters/parameters.f90
@@ -1676,7 +1676,7 @@ subroutine ffdev_params_get_lower_bounds(tmpx)
             case(REALM_VDW_R0)
                 tmpx(id) = 0.5d0
             case(REALM_VDW_ALPHA)
-                tmpx(id) = 13.0d0
+                tmpx(id) = 10.0d0
         end select
     end do
 
@@ -1728,9 +1728,9 @@ subroutine ffdev_params_get_upper_bounds(tmpx)
             case(REALM_IMPR_G)
                 tmpx(id) = 1.05*DEV_PI
             case(REALM_VDW_EPS)
-                tmpx(id) = 1.0d0
+                tmpx(id) = 2.0d0
             case(REALM_VDW_R0)
-                tmpx(id) = 5.0d0
+                tmpx(id) = 6.0d0
             case(REALM_VDW_ALPHA)
                 tmpx(id) = 20.0d0
         end select
